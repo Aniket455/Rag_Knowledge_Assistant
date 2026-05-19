7dffa143af833873bcd74870c8fb624e771963a5
Aniket455 <aniketsgupta0302@gmail.com>
2026-05-19 13:52:55 +0530

    Initial commit - Spring ARag Knowledge Assistant

---
+++ src/main/java/com/spring_ai/Rag_Knowledge_Assistant/RagKnowledgeAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.spring_ai.Rag_Knowledge_Assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagKnowledgeAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagKnowledgeAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_ai/Rag_Knowledge_Assistant/RagKnowledgeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_ai.Rag_Knowledge_Assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagKnowledgeAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
